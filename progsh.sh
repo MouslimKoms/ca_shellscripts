9470897e01e507a689771d129415d407ddbff59e
MouslimKoms <140436051+MouslimKoms@users.noreply.github.com>
2023-11-06 03:23:35 +0100

    Add files via upload

---
+++ progsh.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+echo "le nom de mon fichier $0"
+
+echo "nombre argument dans le script $#"
+
+echo "voici le premier argument du script $1"
+echo "voici le second argument du script $2"
+echo "voici le troisième argument du cript $3"
+
+echo "le nom des sommes total d'arguments au script $*" 
