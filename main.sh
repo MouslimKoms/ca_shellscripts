c5e57e8faf6d6cabee5059ee42212194b9d2d7c7
MouslimKoms <140436051+MouslimKoms@users.noreply.github.com>
2023-11-06 02:29:01 +0100

    variable

--- main.sh
+++ main.sh
@@ -1,6 +1,24 @@
+#!/bin/bash
 
-#ici la commande echo nous affiche le contenue.
+BIRTHDATE="Jan 1, 2000"
+Presents=10
+BIRTHDAY=`date -d "$BIRTHDATE" +%A`
 
-#! /bin/bash
 
-echo "Hello World!" 
+
+
+if [ "$BIRTHDATE" == "Jan 1, 2000" ] ; then
+    echo "BIRTHDATE is correct, it is $BIRTHDATE"
+else
+    echo "BIRTHDATE is incorrect - please retry"
+fi
+if [ $Presents == 10 ] ; then
+    echo "I have received $Presents presents"
+else
+    echo "Presents is incorrect - please retry"
+fi
+if [ "$BIRTHDAY" == "Saturday" ] ; then
+    echo "I was born on a $BIRTHDAY"
+else
+    echo "BIRTHDAY is incorrect - please retry"
+fi
